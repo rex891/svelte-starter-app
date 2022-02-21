75e01c83d4332decf3b62a8966cf5340a82e64de
Joseph Chavez <chavez35@llnl.gov>
2022-02-20 18:57:06 -0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<template lang="pug">
+	main.text-center.p-4
+		h1.text-red-700.text-4xl.uppercase Hello {name}!
+		p
+			| Visit the 
+			a(href="https://svelte.dev/tutorial") Svelte tutorial 
+			| to learn how to build Svelte apps.
+</template>
+
+<style lang="postcss" global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+
+</style>
